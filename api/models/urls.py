223f2d6cbcd89e1d235104896377214c745ce18d
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-06-05 16:33:48 +0100

    Urls Model

---
+++ api/models/urls.py
@@ -0,0 +1,19 @@
+from ..db import db
+from datetime import datetime as dt
+
+
+class Url(db.Model):
+    __tablename__ = 'url'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.Text, nullable=False, unique=True)
+    short_code = db.Column(db.String(20), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, default=dt.datetime.utcnow)
+    last_used_at = db.Column(db.DateTime)
+    click_count = db.Column(db.Integer, default=0)
+    click_source = db.Column(db.String(200))
+
+
+    def __init__(self, short_code, original_url):
+        self.short_code = short_code
+        self.original_url = original_url
