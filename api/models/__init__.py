1f04bea3785d65d3f027cb0c87d50d4ee3824a28
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-06-05 16:40:43 +0100

    models helper file

---
+++ api/models/__init__.py
@@ -0,0 +1,18 @@
+
+from ..db import db
+from flask_restx import abort
+from .users import User
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+from .urls import Url
+
+
+def save(self):
+    db.session.add(self)
+    db.session.commit()
+    
+def update(self):
+    db.session.commit()
+    
+def delete(self):
+        db.session.delete(self)
+        db.session.commit()
