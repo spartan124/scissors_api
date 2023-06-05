1e85149c738df736db9e673e4ee93242bc9ba16b
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-06-05 16:39:38 +0100

    Configuration file

---
+++ api/config/config.py
@@ -0,0 +1,38 @@
+import os
+from datetime import timedelta
+
+from decouple import config
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+class Config:
+    SECRET_KEY = config("SECRET_KEY", "secret")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=120)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=272)
+    JWT_SECRET_KEY = config('JWT_SECRET_KEY')
+    ERROR_INCLUDE_MESSAGE = False
+
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+
+class ProdConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = config('POSTGRESQL')
+
+
+config_dict = {"dev": DevConfig, "prod": ProdConfig, "test": TestConfig}
