ed61bfaef35bdaad4dced44b659b602f735ad4ef
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-06-10 10:24:13 +0100

    modified to abstract some functions.

--- api/resources/urls.py
+++ api/resources/urls.py
@@ -1,19 +1,20 @@
 from datetime import datetime
 import qrcode
-from ..config.config import Config
 from flask_restx import Resource, Namespace, fields, abort
 from flask import make_response, render_template, request, redirect, send_file
-from ..models import Url, save, delete, update
+from ..models import Url, Click, save, delete, update
 from flask_jwt_extended import jwt_required, get_jwt_identity
-import string
-import random
+from ..config.config import cache, limiter
+from .. import limiter
 # from flask_caching import cache
 from io import BytesIO
 import base64
 import requests
-
+from . import is_url_valid, generate_short_code, get_geolocation
 
 url_ns = Namespace("", description="URL Shortener API Operations...")
+
+
 
 url_request_model = url_ns.model('URLRequest', {
     'original_url': fields.String(required=True, description='Original URL'),
@@ -28,23 +29,9 @@ url_response_model = url_ns.model('URLResponse', {
     'created_at': fields.String(required=True, description='Timestamp')
 })
 
-def generate_short_code():
-    """Generate a random shortcode.
-    """
-    chars = string.ascii_letters + string.digits
-    short_code = ''.join(random.choice(chars) for _ in range(6))
-    return short_code
 
-def get_geolocation(ip_address):
-        response = requests.get(f'{Config.IPSTACK_API_URL}/{ip_address}?access_key={Config.IPSTACK_ACCESS_KEY}')
-        if response.status_code == 200:
-            data = response.json()
-            if 'city' in data and 'country_name' in data:
-                return f'{data["city"]}, {data["country_name"]}'
-        return ''
-
+@limiter.limit("10/minute")
 @url_ns.route('/shorten')
-
 class URLShortener(Resource):
     @url_ns.expect(url_request_model, validate=True)
     @jwt_required()
@@ -53,6 +40,10 @@ class URLShortener(Resource):
         Create a new shortened URL.
         """
         original_url = request.json.get('original_url')
+        
+        if not is_url_valid(original_url):
+            return {'message': 'Invalid URL'}, 400
+        
         short_code = request.json.get('short_code')
         user_id = get_jwt_identity()
         
@@ -86,28 +77,12 @@ class URLShortener(Resource):
         
         shortened_url = f"{base_url}{short_code}"
         
-        qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-        )
-        qr.add_data(shortened_url)
-        qr.make(fit=True)
-        qr_image = qr.make_image(fill_color="black", back_color="white")
-
-        # Save QR code image to a BytesIO object
-        qr_buffer = BytesIO()
-        qr_image.save(qr_buffer)
-        qr_buffer.seek(0)
-
-        response = make_response(send_file(qr_buffer, mimetype='image/png'))
-        response.headers.set('Content-Disposition', 'attachment', filename='qrcode.png')
         
         return {'shortened_url': shortened_url}, 201
     
     @url_ns.marshal_with(url_response_model)
     @jwt_required()
+    @cache.cached(timeout=60)
     def get(self):
         """Get all shortened urls
 
@@ -116,11 +91,11 @@ class URLShortener(Resource):
         """
         url = Url.query.all()
         return url
-    
+
+@limiter.limit("5/second")    
 @url_ns.route('/<short_code>')
-class URLRedirect(Resource):
-    # @cache.cached(timeout=60) #cache the result for 60 seconds
-   
+class URLRedirect(Resource):  
+    @cache.cached(timeout=60)
     def get(self, short_code):
         """
         Redirect to the original url for the given short code.
@@ -129,11 +104,16 @@ class URLRedirect(Resource):
             short_code (_type_): Shortened URL
         """
         url = Url.query.filter_by(short_code=short_code).first()
+        
         if url:
+            ip_address = request.remote_addr
+            geolocation_data = get_geolocation(ip_address)
+            click = Click(url_id=url.id, click_source=geolocation_data)
+            save(click)
+            
             original_url = url.original_url
             url.last_used_at = datetime.utcnow()
             url.click_count += 1
-            url.click_source = get_geolocation(request.remote_addr)
             update(url)
             return redirect(original_url)
         
@@ -143,6 +123,7 @@ class URLRedirect(Resource):
 @url_ns.route("/shortened/<short_code>")   
 class OriginalUrl(Resource):
     @jwt_required()
+    @cache.cached(timeout=60)
     def get(self, short_code):
         url = Url.query.filter_by(short_code=short_code).first()
         
@@ -159,26 +140,67 @@ class OriginalUrl(Resource):
             return {"message": "Url successfully deleted"}
         abort(404, message="Url not found")
 
-@url_ns.route('/analytics/<short_code>')
-class ShortUrlAnalytics(Resource):
+@url_ns.route("/<short_code>/qrcode")
+class GenerateQrCode(Resource):
     @jwt_required()
+    @cache.cached(timeout=60)
     def get(self, short_code):
         
         url = Url.query.filter_by(short_code=short_code).first()
         
+        base_url = request.host_url
+        shortened_url = f"{base_url}{short_code}"
+        if url:
+            qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+            )
+            qr.add_data(shortened_url)
+            qr.make(fit=True)
+            qr_image = qr.make_image(fill_color="black", back_color="white")
+
+            # Save QR code image to a BytesIO object
+            qr_buffer = BytesIO()
+            qr_image.save(qr_buffer)
+            qr_buffer.seek(0)
+
+            response = make_response(send_file(qr_buffer, mimetype='image/png'))
+            response.headers.set('Content-Disposition', 'attachment', filename='qrcode.png')
+            return response
+        
+    
+@url_ns.route('/analytics/<short_code>')
+class ShortUrlAnalytics(Resource):
+    @jwt_required()
+    @cache.cached(timeout=60)
+    def get(self, short_code):
+        
+        url = Url.query.filter_by(short_code=short_code).first()
+        
+        clicks = Click.query.filter_by(url_id=url.id).all()
+        click_data = {}
+        for click in clicks:
+            if click.click_source in click_data:
+                click_data[click.click_source] += 1
+            else:
+                click_data[click.click_source] = 1
+    
         if url:
             return {
                 'original_url': url.original_url,
                 'click_count': url.click_count,
                 'created_at': url.created_at.strftime("%Y-%m-%d %H:%M:%S"),
                 'last_used_at': url.last_used_at.strftime("%Y-%m-%d %H:%M:%S") if url.last_used_at else None,
-                'click_source': url.click_source
+                'click_data': click_data
   
             }
         return {"message": "URL not found"}, 404
 @url_ns.route('/history')    
 class UrlHistory(Resource):
     @jwt_required()
+    @cache.cached(timeout=60)
     def get(self):
         """
         Get a specifi user's url link history
@@ -191,12 +213,8 @@ class UrlHistory(Resource):
         
         for url in urls:
             history.append({
-                'short_code': url.short_code,
-                'original_url': url.original_url,
-                'created_at': url.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                'last_used_at': url.last_used_at.strftime("%Y-%m-%d %H:%M:%S") if url.last_used_at else None,
-                'click_count': url.click_count,
-                'click_source': url.click_source
+                'shortened_url': f"{request.host_url}{url.short_code}",
+                'original_url': url.original_url
             })
 
         return {'history': history}
