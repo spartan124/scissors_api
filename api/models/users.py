7a0e5f25728fd6c72f17ca13c5a154f0d1ee8819
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-06-19 12:45:25 +0100

    removed phone number from user model

--- api/models/users.py
+++ api/models/users.py
@@ -6,7 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(80), nullable=False, unique=True)
-    phone = db.Column(db.String(15), nullable=True)
     full_name = db.Column(db.String(256), unique=False, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     urls = db.relationship('Url', backref='users', lazy=True)
