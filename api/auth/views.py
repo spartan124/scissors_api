e0087642c700ae5ba07af5e148dc858308da9ab8
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-06-19 12:44:16 +0100

    fixed issues with blacklisting tokens

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,8 +1,11 @@
-
-from flask_jwt_extended import (create_access_token, create_refresh_token,
+import json
+from flask import g
+from flask_jwt_extended import (create_access_token, create_refresh_token, get_jwt,
                                 get_jwt_identity, jwt_required)
 from flask_restx import Namespace, Resource, abort, fields
 from werkzeug.security import check_password_hash, generate_password_hash
+from ..config.config import cache
+from api.auth import blacklist_token, jwt_required_with_blacklist, jwt_refresh_token_required_with_blacklist
 
 from ..models import User, delete, save, update
 
@@ -14,7 +17,6 @@ signup_model = namespace.model(
         "full_name": fields.String(required=True, description="User's full name"),
         "username": fields.String(required=True, description="User's username"),
         "email": fields.String(required=True, description="User's email address"),
-        "phone": fields.String(required=True, description="User's Phone number"),
         "password": fields.String(required=True, description="User's Account Password"),
        
 
@@ -28,7 +30,6 @@ user_model = namespace.model(
         "full_name": fields.String(required=True, description="User's name"),
         "email": fields.String(required=True, description="User's email"),
         "username": fields.String(required=True, description="User's username"),
-        "phone": fields.String(required=True, description="User's Phone number")
 
     }
 )
@@ -54,7 +55,6 @@ class Signup(Resource):
         full_name = data["full_name"]
         username = data['username']
         email = data['email']
-        phone = data['phone']
         password_hash = generate_password_hash(data["password"])
         
         new_user = User.query.filter_by(username=username).first()
@@ -66,7 +66,6 @@ class Signup(Resource):
             full_name = full_name,
             username = username,
             email = email,
-            phone = phone,
             password_hash = password_hash
         )
         
@@ -89,12 +88,12 @@ class Login(Resource):
         user = User.query.filter_by(email=email).first()
         
         if (user is not None) and (check_password_hash(user.password_hash, password)):
-            access_token = create_access_token(identity=user.email)
-            refresh_token = create_refresh_token(identity=user.email)
-            
+            access_token = create_access_token(identity={'id': user.id, 'email': user.email})
+            refresh_token = create_refresh_token(identity={'id': user.id, 'email': user.email})
             response = {
                 'access_token': access_token,
-                'refresh_token': refresh_token
+                'refresh_token': refresh_token,
+                'username': user.username
             }
             
             return response, 201
@@ -104,12 +103,36 @@ class Login(Resource):
         
 @namespace.route('/refresh')
 class Refresh(Resource):
-    @jwt_required(refresh=True)
+    @jwt_refresh_token_required_with_blacklist
     def post(self):
-        email = get_jwt_identity()
-        access_token = create_access_token(identity=email)
+        user = get_jwt_identity()
+        id = user['id']
+        email = user['email']
+        access_token = create_access_token(identity={'id': id, 'email': email})
         response = {
             'access_token':access_token
         }
         return response, 201
 
+@namespace.route('/logout')
+class Logout(Resource):
+    @jwt_required_with_blacklist
+    def post(self):
+        """
+        Logout and Blacklist jwt_token
+        """
+        jwt_token = get_jwt()
+        blacklist_token(json.dumps(jwt_token))
+        return {"message": "Logged out successfully!"}, 200
+@namespace.route('/user')
+class UserData(Resource):
+    @jwt_required_with_blacklist
+    @cache.cached(timeout=60)
+    def get(self):
+        """Get logged in user
+        """
+        user= get_jwt_identity()
+        email = user['email']
+        userdata = User.query.filter_by(email=email).first()
+        if userdata is not None:
+            return {'username': userdata.username}
