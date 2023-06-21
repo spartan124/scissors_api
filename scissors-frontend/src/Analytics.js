d16ec68092f2829b940e5ce711d41c4d607e6dad
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-06-21 13:05:41 +0100

    url analytics

--- scissors-frontend/src/Analytics.js
+++ scissors-frontend/src/Analytics.js
@@ -1,61 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import api from './api';
-import { Bar } from 'react-chartjs-2';
 import { useParams } from 'react-router-dom';
 
-
-function AnalyticsPage({ match }) {
-  const [analyticsData, setAnalyticsData] = useState(null);
+function AnalyticsPage() {
+  const [imageSrc, setImageSrc] = useState(null);
   const [error, setError] = useState(null);
   const { shortCode } = useParams();
 
-
   useEffect(() => {
-    const fetchAnalyticsData = async () => {
+    const fetchAnalyticsImage = async () => {
       try {
-        const response = await api.get(`/analytics/${shortCode}`);
-        setAnalyticsData(response.data);
+        const response = await api.get(`/analytics/${shortCode}`, {
+          responseType: 'arraybuffer', // Receive response as array buffer
+        });
+
+        // Convert the array buffer to base64 data URL
+        const imageSrc = `data:image/png;base64,${btoa(
+          String.fromCharCode(...new Uint8Array(response.data))
+        )}`;
+
+        setImageSrc(imageSrc);
       } catch (error) {
         setError('Failed to fetch analytics data');
         console.error('Failed to fetch analytics data:', error);
       }
     };
-    fetchAnalyticsData();
-  }, [shortCode]);
 
- 
+    fetchAnalyticsImage();
+  }, [shortCode]);
 
   return (
     <div>
       <h1>Analytics for Shortened URL: {shortCode}</h1>
-      {analyticsData ? (
-        <div>
-          {/* <p>Original URL: {analyticsData.original_url}</p>
-          <p>Click Count: {analyticsData.click_count}</p>
-          <p>Created At: {analyticsData.created_at}</p>
-          <p>Last Used At: {analyticsData.last_used_at}</p>
-          <h2>Click Data</h2> */}
-          <Bar
-            data={{
-              labels: Object.keys(analyticsData.click_data),
-              datasets: [
-                {
-                  label: 'Clicks per Location',
-                  data: Object.values(analyticsData.click_data),
-                  backgroundColor: 'rgba(75, 192, 192, 0.6)',
-                },
-              ],
-            }}
-            options={{
-              scales: {
-                y: {
-                  beginAtZero: true,
-                  stepSize: 1,
-                },
-              },
-            }}
-          />
-        </div>
+      {imageSrc ? (
+        <img src={imageSrc} alt="Clicks per Location" />
       ) : (
         <p>Loading analytics data...</p>
       )}
