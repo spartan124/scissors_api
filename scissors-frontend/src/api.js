6b957fda9d0631f0bdb748be9b2e76cecde1539d
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-06-29 14:30:07 +0100

    fixed backend server linkage to react

--- scissors-frontend/src/api.js
+++ scissors-frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://www.scix.me/'
   });
 
   api.interceptors.request.use((config) => {
